2ffd346577092cfcb0f113c2900bd261c9387da7
Thejas A <thejas2246@gmail.com>
2025-05-21 13:09:27 +0530

    Add there buttons for rock paper scissors

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-// the computer choose three random numbers
-// if number is 1 return rock 
-// if the number is 2 return paper 
-// if the number is 3 return scissors
-
 function getComputerChoice(){
 const randomNumber = Math.floor(Math.random()*3)+1;
 if(randomNumber === 1) return "Rock";
 else if(randomNumber === 2) return "Paper";
 else return "Scissors";
 }
-//the function uses prompt to get the users choice
 
 function getHumanChoice(){
     const humanChoice = prompt("Select Rock Paper or Scissors");
@@ -71,4 +65,3 @@ function playGame(){
         console.log('Computer is the winner');
     }
 }
-playGame();
